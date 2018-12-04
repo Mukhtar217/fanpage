d5ed1165e7f045921c4711d30b66d2c2712f6339
Mukhtar Mahdi <mukhtar.mahdi217@gmail.com>
2018-12-04 04:52:26 +0000

    jQuery

---
+++ js/script.js
@@ -0,0 +1,253 @@
+// JavaScript File
+/* global $ */
+
+function bA(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Barry Allen");
+    $(".person").attr("src","https://cdn-images-1.medium.com/max/1024/1*i_7FctETvH1okLarFDpFRw.jpeg");
+    $(".text").text("Barry Allen also known as The Flash is a Forensic Scientist from Central City who works for The Central City Police Department. His parents are Henry Allen and the late Nora Allen. He is also a Metahuman and one of the first of his kind, having received unique powers, most notably Superhuman Speed.");
+    $("#nCR").css("display","inline");
+}
+
+$(".bA").click(function(){
+bA();
+});
+
+function cR(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Cisco Ramon");
+    $(".person").attr("src","https://nerdist.com/wp-content/uploads/2015/05/20150519_HomeGeekonomicsFlashFashion-970x545.jpg");
+    $(".text").text("Cisco Ramon Born Francisco Ramon also known as Vibe is an expert mechanical engineering genius who works for S.T.A.R. Labs Central City Branch.");
+    $("#nCS").css("display","inline");
+}
+
+$(".cR").click(function(){
+cR();
+});
+
+function cS(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Dr. Caitlin Snow");
+    $(".person").attr("src","http://digitalspyuk.cdnds.net/17/41/980x490/landscape-1507644004-01-11-the-flash-reborn-s04.jpeg");
+    $(".text").text("Caitlin Snow also known as Killer Frost is a highly intelligent bio engineering expert scientist who works at S.T.A.R. Labs Central City Branch. She is also from Central City as well she recently lost her fiance Ronnie Raymond during the recent explosion at S.T.A.R. Labs with The Particle Accelerator.");
+    $("#nJW").css("display","inline");
+}
+
+$(".cS").click(function(){
+cS();
+});
+
+function jW(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Detective Joe West");
+    $(".person").attr("src","https://cdn3.movieweb.com/i/article/QOCUpWXfhSAr313qQDz4Vl4HKaC0Z0/798:50/The-Flash-Featurette-Introduces-Detective-Joe-West.jpg");
+    $(".text").text("Joe West is the Father of Iris West and the Adoptive Father of Barry Allen. He's also a Detective of the Central City Police Department.");
+    $("#nHW").css("display","inline");
+}
+
+$(".jW").click(function(){
+jW();
+});
+
+function hW(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Dr. Harrison Wells");
+    $(".person").attr("src","http://media.comicbook.com/2017/10/the-flash-season-4-harry-wells-1040489.jpg");
+    $(".text").text("Harrison Wells form Earth 2, also known as Harry is the father of Jesse Quick. He recently traveled through The Multiverse Breach currently connecting Earth 2 and Earth 1 and joined forces with The Flash and his Team in battling Zoom and saving his Daughter and his own world from the threat of Zoom.");
+    $("#nIW").css("display","inline");
+}
+
+$(".hW").click(function(){
+hW();
+});
+
+function iW(){
+    $(".next").css("display","none");
+    $(".titleL").text("Team Flash");
+    $(".name").text("Iris West Allen");
+    $(".person").attr("src","https://tribzap2it.files.wordpress.com/2015/11/candice-patton-the-flash-thecw.jpg?w=900");
+    $(".text").text("Iris West is the daughter of Joe West and the late Francine West an older sister of Wally West. She is a reporter for Central City Picture News and the Wife of Barry Allen.");
+    $("#nTA").css("display","inline");
+}
+
+$(".iW").click(function(){
+iW();
+});
+
+
+
+function tA(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Team Arrow");
+    $(".person").attr("src","https://static.comicvine.com/uploads/original/11113/111139268/5096727-7516297326-45887.png");
+    $(".text").text("Team Arrow is a vigilante team dedicated to fighting criminals, mainly operating in Star City.");
+    $("#nSG").css("display","inline");
+}
+
+$(".tA").click(function(){
+tA();
+});
+
+function sG(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Supergirl");
+    $(".person").attr("src","https://cdn3-www.comingsoon.net/assets/uploads/2017/09/This-Fall-on-The-CW-The-CW-YouTube.jpg");
+    $(".text").text("Supergirl is an alien from Earth-38. Strong, bulletproof, and friendly. Flash meets supergirl when he entered her Earth by accident.");
+    $("#nLG").css("display","inline");
+}
+
+$(".sG").click(function(){
+sG();
+});
+
+function lG(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("The Legends");
+    $(".person").attr("src","https://1.bp.blogspot.com/-wi7eHNX3VnQ/WSdGukmiLVI/AAAAAAAATEI/YmGRj0xde0wTe338n9IKH8WEapg2PUVuACLcB/s1600/DAs3CD6WAAAp4Mq%2B%255Bwww.imagesplitter.net%255D.jpeg");
+    $(".text").text("A group of miscasts come together to become Legends. The Legends have the abilty to travel anywhere in time and have the job to protect the timeline.");
+    $("#nLS").css("display","inline");
+}
+
+$(".lG").click(function(){
+lG();
+});
+
+function lS(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Leonard Snart");
+    $(".person").attr("src","https://pmcvariety.files.wordpress.com/2015/12/captain-cold-dcs-legend-of-tomorrow-cropped.jpg?w=1000");
+    $(".text").text("Leonard Snart also known as Captain Cold is well known and highly skilled professional thief of Central City. His partner is Heat Wave. He is the son of a Former Bent Central City Cop and older brother of Lisa Snart. At a young age he learned to study the police to make sure he always got away before they could respond.");
+    $("#nFS").css("display","inline");
+}
+
+$(".lS").click(function(){
+lS();
+});
+
+function fS(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Firestorm");
+    $(".person").attr("src","https://www.syfy.com/sites/syfy/files/wire/legacy/The-Flash-Robbie-Amell-Firestorm-Poster.jpg");
+    $(".text").text('Ronald "Ronnie" Raymond also known as Firestorm was Mechanical Engineer who worked at S.T.A.R. Labs in Central City. He was engaged to be married to his fiance Caitlin Snow another Team Member of S.T.A.R. Labs.');
+    $("#nJQ").css("display","inline");
+}
+
+$(".fS").click(function(){
+fS();
+});
+
+function jQ(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Jesse Quick");
+    $(".person").attr("src","https://pmctvline2.files.wordpress.com/2017/02/the-flash-violett-beane.jpg?w=620&h=420&crop=1");
+    $(".text").text("Jesse Chambers Wells also known Jesse Quick is a Metahuman Speedster from Earth 2. She is from Central City and is The Daughter of Harrison Wells.");
+    $("#nKF").css("display","inline");
+}
+
+$(".jQ").click(function(){
+jQ();
+});
+
+function kF(){
+    $(".next").css("display","none");
+    $(".titleL").text("Allies");
+    $(".name").text("Kid Flash");
+    $(".person").attr("src","https://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2018/01/keiynan-lonsdale-the-flash.jpg?itok=WsNpnXxk");
+    $(".text").text("Wallace West also known as Kid Flash is a Metahuman Speedster. He is also the Son of Joe West and Francine West. He is the younger brother of Iris West.");
+    $("#nRF").css("display","inline");
+}
+
+$(".kF").click(function(){
+kF();
+});
+
+
+
+function rF(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("Reverse Flash");
+    $(".person").attr("src","https://vignette.wikia.nocookie.net/marvel_dc/images/a/af/Reverse-Flash_Arrow_0003.png/revision/latest?cb=20150430091408");
+    $(".text").text("Born in the Year 2151, Eobard Thawne also known as The Reverse Flash is a Mysterious and Extremely Dangerous Metahuman Speedster with a Former Connection to The Speed Force which is now only temporary and extremely knowledgeable of it's Powers. He is the Archenemy of The Flash and one of heroes most well known Villains.");
+    $("#nZO").css("display","inline");
+}
+
+$(".rF").click(function(){
+rF();
+});
+
+function zO(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("Zoom");
+    $(".person").attr("src","https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Zoom_%28Arroverse%29.png/165px-Zoom_%28Arroverse%29.png");
+    $(".text").text("Hunter Zolomon also known as Zoom is an extremely powerful Villain and mysterious Metahuman Speedster from Earth 2. He was created by Harrison Wells of Earth 2 when the S.T.A.R. Labs's Particle Accelerator in Central City was turned on and exploded creating Metahumans such as himself and many others.");
+    $("#nST").css("display","inline");
+}
+
+$(".zO").click(function(){
+zO();
+});
+
+function sT(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("Savitar");
+    $(".person").attr("src","https://static0.srcdn.com/wordpress/wp-content/uploads/2017/01/savitar-the-flash-season-3.jpg");
+    $(".text").text("Barry Allen also known as Savitar is The 1st Metahuman Speedster known in The Multiverse to have ever connect to The Speed Force known to many other Speedsters as The God of Speed.");
+    $("#nTT").css("display","inline");
+}
+
+$(".sT").click(function(){
+sT();
+});
+
+function tT(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("The Thinker");
+    $(".person").attr("src","https://imgix.bustle.com/uploads/image/2017/10/16/fdd70142-ef84-4632-b743-084016d0a65f-the-thinker.png?w=970&h=582&fit=crop&crop=faces&auto=format&q=70");
+    $(".text").text("Clifford DeVoe also known as The Thinker is a Metahuman Villain of The Flash from Earth 1. He has extreme knowledge and he is the smartest man ever to live.");
+    $("#nCD").css("display","inline");
+}
+
+$(".tT").click(function(){
+tT();
+});
+
+function cD(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("Cicada");
+    $(".person").attr("src","https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/10/the_flash_cicada.jpg?itok=ht0HRVJb");
+    $(".text").text("Orlin Dwyer also known as Cicada is an extremely powerful and dangerous Metahuman. He's currently hunting his fellow Metahumans one by one killing them since he blames them for all the pain they have caused him and his family in their lives essentially destroying his family resulting his family dying.");
+    $("#nTL").css("display","inline");
+}
+
+$(".cD").click(function(){
+cD();
+});
+
+function tL(){
+    $(".next").css("display","none");
+    $(".titleL").text("Villains");
+    $(".name").text("Timeline");
+    $(".person").attr("src","https://images-cdn.9gag.com/photo/app1RRb_700b.jpg");
+    $(".text").text("Ever since flashpoint, the Timeline has been a real problem for The Flash as he tends to change it a lot; causing lots of problems for him.");
+    $("#nBA").css("display","inline");
+}
+
+$(".tL").click(function(){
+tL();
+});
